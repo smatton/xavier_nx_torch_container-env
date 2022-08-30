87a5c7324d4eaa0b050de010cbcea41fc805fca8
Scott Matton <scott.matton@brambles.com>
2022-08-29 22:47:44 -0400

    initial dockerfile

---
+++ docker/entrypoint.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+
+
+jupyter notebook --no-browser --ip=0.0.0.0 --port=8080 --alow-root
---
+++ runcontainer.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+
+docker run -it --rm --runtime nvidia --network host torchngc:v0.0.0
